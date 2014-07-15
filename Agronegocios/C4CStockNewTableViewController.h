a5309c6b092b5ea45533e6cc9835d37ec279dd72
David Alméciga <wdavid@dagrinchi.com>
2014-07-14 21:46:35 -0500

    navegacion: modals en cada opcion

---
+++ Agronegocios/C4CStockNewTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  C4CStockNewTableViewController.h
+//  Agronegocios
+//
+//  Created by David Almeciga on 7/14/14.
+//  Copyright (c) 2014 COOL4CODE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface C4CStockNewTableViewController : UITableViewController
+
+- (IBAction)unwindProductList:(UIStoryboardSegue *)segue;
+
+@end
---
+++ Agronegocios/C4CWhoamiViewController.h
@@ -0,0 +1,17 @@
+//
+//  C4CWhoamiViewController.h
+//  Agronegocios
+//
+//  Created by David Almeciga on 7/14/14.
+//  Copyright (c) 2014 COOL4CODE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface C4CWhoamiViewController : UIViewController
+
+- (IBAction)unwindFarmer:(UIStoryboardSegue *)segue;
+
+- (IBAction)unwindCustomer:(UIStoryboardSegue *)segue;
+
+@end
