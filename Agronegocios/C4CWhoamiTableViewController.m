f10bc70d09d4cda83efe1973af45744a9312cdef
David Alméciga <wdavid@dagrinchi.com>
2014-08-25 19:04:46 -0500

    Fix table views

---
+++ Agronegocios/C4CWhoamiTableViewController.m
@@ -0,0 +1,37 @@
+//
+//  C4CWhoamiTableViewController.m
+//  Agronegocios
+//
+//  Created by David Almeciga on 8/18/14.
+//  Copyright (c) 2014 COOL4CODE. All rights reserved.
+//
+
+#import "C4CWhoamiTableViewController.h"
+
+@interface C4CWhoamiTableViewController ()
+
+@end
+
+@implementation C4CWhoamiTableViewController
+
+- (IBAction)unwindFarmer:(UIStoryboardSegue *)segue {
+    
+}
+
+- (IBAction)unwindCustomer:(UIStoryboardSegue *)segue {
+    
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+//    UITableViewCell *farmerCell = [self.tableView dequeueReusableCellWithIdentifier:@"farmerCell"];
+//    [farmerCell set
+    
+//    UITableViewCell *unitPickerCell = [self.tableView dequeueReusableCellWithIdentifier:_unitPickerCellID];
+//    self.unitPickerCellRowHeight = CGRectGetHeight(unitPickerCell.frame);
+    
+}
+
+@end
